16e8c4c0e4df974a6e587b42338a608a871e33e1
Andreas Ommundsen <anthomm@live.com>
2023-06-28 15:05:05 +0200

    fix: add cors package; add queries to search;

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ import {applicationDefault, initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
 import morgan from "morgan";
 import http from "http-status-codes";
+import cors from "cors"
 
 const app = express()
 app.use(morgan("tiny"))
 app.use(express.json())
+app.use(express.urlencoded({extended: true})) // Allows parsing nested objects.
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Local React URL
+}))
 
 initializeApp({
     credential: applicationDefault()
@@ -17,10 +23,11 @@ const db = getFirestore();
 // CREATE
 app.post("/user", async (req, res) => {
 
-    const {name, age} = req.body
+    const {name, age, consent} = req.body
     const user = {
         name: name,
         age: age,
+        consent: consent,
         created: now(),
     }
 
@@ -52,10 +59,11 @@ app.get("/user/:id", async (req, res) => {
 app.patch("/user/:id", async (req, res) => {
 
     const id = req.params.id
-    const {name, age} = req.body
+    const {name, age, consent} = req.body
     const update = {
         name: name,
         age: age,
+        consent: consent,
         updated: now(),
     }
 
@@ -84,7 +92,21 @@ app.delete("/user/:id", async (req, res) => {
 // SEARCH
 app.get("/user", async (req, res) => {
 
-    await db.collection("user").get()
+    let search = await db.collection("user")
+    if (req.query.q) {
+        const qs = (req.query.q instanceof Array) ? req.query.q : [req.query.q]
+        for (const q of qs) {
+            try {
+                let [key, operator, value] = q.split(",")
+                value = parse(value)
+                // noinspection JSCheckFunctionSignatures
+                search = search.where(key, operator, value)
+            } catch (e) {
+                return errorResponse(e, res)
+            }
+        }
+    }
+    await search.get()
         .then((snapshots) => {
             let docs = []
             snapshots.forEach((doc) => {
@@ -131,5 +153,18 @@ function exists(x) {
 }
 
 function errorResponse(e, res) {
-    res.status(e.status ?? http.BAD_REQUEST).send(defaultErrorMessage(e))
+    if (e.code === 9) { // Firestore catcher
+        e = new Error("Queries containing both equality & inequality operators on different fields are not allowed. Example: 'name==James' && 'age>=23'")
+        e.status = http.BAD_REQUEST
+    }
+    e.status ??= http.BAD_REQUEST
+    res.status(e.status).send(defaultErrorMessage(e))
+}
+
+function parse(value) {
+    try {
+        return JSON.parse(value);
+    } catch (_) {
+        return value;
+    }
 }
